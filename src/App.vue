293184b03b3fed4e6f369be0d2ccb34a8ec4196e
Dan13615 <daniil.stepanov@epitech.eu>
2024-05-05 18:31:14 +0200

    [~] Welcome view, background, footer/header

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,26 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-    <div>
-        <div class="bg-gradient-to-b from-slate-800 to-slate-900">
-            <header class="p-2 w-full flex flex-row justify-between">
-                <div>
-                    <p class="text-2xl">INSERT LOGO</p>
-                </div>
-                <div>
-                    <nav class="w-full flex gap-5">
-                        <RouterLink to="/">Home</RouterLink>
-                        <RouterLink to="/projects">My Projects</RouterLink>
-                        <RouterLink to="/contact">Contact</RouterLink>
-                    </nav>
-                </div>
-            </header>
-        </div>
-        <div class="min-h-screen">
-            <RouterView class="flex justify-center transition-all pt-36" />
-        </div>
-        <div class="bg-gradient-to-t from-slate-800 to-slate-900">
-            <footer class="p-2 w-full text-left">
-                <p>&copy; 2024 Daniil Stepanov</p>
-            </footer>
-        </div>
+    <div class="bg-gradient-to-b from-gray-400 p-2">
+        <header class="p-2 w-full flex flex-row justify-between">
+            <div>
+                <p class="text-2xl">INSERT LOGO</p>
+            </div>
+            <div>
+                <nav class="font-bold w-full flex gap-5">
+                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/projects">My Projects</RouterLink>
+                    <RouterLink to="/contact">Contact</RouterLink>
+                </nav>
+            </div>
+        </header>
+    </div>
+    <div class="min-h-screen flex justify-center transition-all pt-64">
+        <RouterView />
+    </div>
+    <div class="bg-gradient-to-t from-gray-400 p-2">
+        <footer class="p-2 w-full text-left">
+            <p class="font-bold">&copy; 2024 Daniil Stepanov</p>
+        </footer>
     </div>
 </template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,8 +1,10 @@
 <template>
     <WelcomeItem>
-        <div class="text-center">
-            <p class="text-4xl">Welcome to my Portfolio</p>
-            <p class="text-base">My name is Daniil Stepanov and I am an embedded developper</p>
+        <div class="bg-gray-300 bg-opacity-60 p-5 rounded-xl">
+            <div class="text-center">
+                <p class="md:text-xl lg:text-4xl font-black">Welcome to my Portfolio</p>
+                <p class="text-base">My name is Daniil Stepanov and I am an embedded developper</p>
+            </div>
         </div>
     </WelcomeItem>
 </template>
