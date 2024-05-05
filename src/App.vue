a308f03f89660b9b1b69a732754b97293935d3c4
Dan13615 <daniil.stepanov@epitech.eu>
2024-05-05 17:20:02 +0200

    [+] init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+    <div>
+        <div class="bg-gradient-to-b from-slate-800 to-slate-900">
+            <header class="p-2 w-full flex flex-row justify-between">
+                <div>
+                    <p class="text-2xl">INSERT LOGO</p>
+                </div>
+                <div>
+                    <nav class="w-full flex gap-5">
+                        <RouterLink to="/">Home</RouterLink>
+                        <RouterLink to="/projects">My Projects</RouterLink>
+                        <RouterLink to="/contact">Contact</RouterLink>
+                    </nav>
+                </div>
+            </header>
+        </div>
+        <div class="min-h-screen">
+            <RouterView class="flex justify-center transition-all pt-36" />
+        </div>
+        <div class="bg-gradient-to-t from-slate-800 to-slate-900">
+            <footer class="p-2 w-full text-left">
+                <p>&copy; 2024 Daniil Stepanov</p>
+            </footer>
+        </div>
+    </div>
+</template>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,10 @@
+<template>
+    <WelcomeItem>
+        <div class="text-center">
+            <p class="text-4xl">Welcome to my Portfolio</p>
+            <p class="text-base">My name is Daniil Stepanov and I am an embedded developper</p>
+        </div>
+    </WelcomeItem>
+</template>
+
+<script setup></script>
