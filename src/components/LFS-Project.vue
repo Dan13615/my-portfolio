cdebcdd77693db6bba36b8e465005f71e7ebf17b
Dan13615 <daniil.stepanov@epitech.eu>
2024-05-08 19:44:34 +0200

    [~] made projects responsive and finished my first project

--- src/components/LFS-Project.vue
+++ src/components/LFS-Project.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="box">
-        <p class="text-4xl pb-5 title">{{ project.title }}</p>
-        <p class="text-xl">{{ project.description }}</p>
+        <p class="text-xl md:text-2xl lg:text-4xl pb-5 title">{{ project.title }}</p>
+        <p class="text-base md:text-lg lg:text-2xl">{{ project.description }}</p>
         <img class="picture" :src="project.picture" alt="Project Picture" />
-        <a class="button text-xl" :href="project.githubLink">GitHub Link</a>
+        <a class="button text-base md:text-lg lg:text-2xl" :href="project.githubLink">GitHub Link</a>
     </div>
 </template>
 
@@ -58,5 +58,24 @@ export default {
 .picture {
     width: 40%;
     height: 40%;
+}
+
+@media (max-width: 1024px) {
+    .button {
+        display: inline-block;
+        padding: 0.6rem 1.5rem;
+        background: #0ef;
+        border-radius: 4rem;
+        box-shadow: 0 0 1rem #0ef;
+        color: rgb(255, 255, 255);
+        letter-spacing: 0.1rem;
+        font-weight: 1000;
+        transition: 0.5s ease;
+    }
+
+    .picture {
+        width: 60%;
+        height: 60%;
+    }
 }
 </style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,13 +1,13 @@
 <template>
     <main>
         <div class="title-container">
-            <div class="title font-bold text-xl sm:text-2xl md:text-5xl lg:text-7xl">
-                <p>My personnal<span class="span text-xl sm:text-3xl md:text-6xl lg:text-8xl"> Projects !</span></p>
+            <div class="title font-bold text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
+                <p>My personnal<span class="span text-4xl sm:text-5xl md:text-6xl lg:text-8xl"> Projects !</span></p>
             </div>
         </div>
 
         <div class="projects">
-            <LFS-Project />
+            <LFSProject />
         </div>
     </main>
 </template>
@@ -39,5 +39,19 @@ import LFSProject from '@/components/LFS-Project.vue';
     margin-left: 50%;
     display: flex;
     flex-direction: column;
+}
+
+@media (max-width: 1024px) {
+    .title-container {
+        position: static;
+        right: auto;
+        top: auto;
+        left: auto;
+        margin-bottom: 8%;
+    }
+
+    .projects {
+        margin-left: 0;
+    }
 }
 </style>
