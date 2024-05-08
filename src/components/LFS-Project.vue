1ae3c3cb5cbfba6c444fc41413c08c1a0198f3cb
Dan13615 <daniil.stepanov@epitech.eu>
2024-05-08 19:06:08 +0200

    [+] Working on the projects section

---
+++ src/components/LFS-Project.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="box">
+        <p class="text-4xl pb-5 title">{{ project.title }}</p>
+        <p class="text-xl">{{ project.description }}</p>
+        <img class="picture" :src="project.picture" alt="Project Picture" />
+        <a class="button text-xl" :href="project.githubLink">GitHub Link</a>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            project: {
+                title: 'Linux From Scratch Project',
+                description: 'Linux From Scratch (LFS) is a project that provides you with step-by-step instructions for building your own custom Linux system, entirely from source code.',
+                githubLink: 'https://github.com/Dan13615',
+                picture: '/src/assets/lfs.png'
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    width: 90%;
+    padding: 1rem;
+    color: white;
+    background: rgba(57, 62, 70, 0.8);
+    border-radius: 0.8rem;
+    margin: 1rem;
+    resize: none;
+}
+
+.title {
+    color: #0ef;
+    font-weight: bold;
+}
+
+.button {
+    display: inline-block;
+    padding: 0.8rem 2rem;
+    background: #0ef;
+    border-radius: 4rem;
+    box-shadow: 0 0 1rem #0ef;
+    color: rgb(255, 255, 255);
+    letter-spacing: 0.1rem;
+    font-weight: 1000;
+    transition: 0.5s ease;
+}
+
+.picture {
+    width: 40%;
+    height: 40%;
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,9 +1,43 @@
 <template>
     <main>
-        <TheWelcome />
+        <div class="title-container">
+            <div class="title font-bold text-xl sm:text-2xl md:text-5xl lg:text-7xl">
+                <p>My personnal<span class="span text-xl sm:text-3xl md:text-6xl lg:text-8xl"> Projects !</span></p>
+            </div>
+        </div>
+
+        <div class="projects">
+            <LFS-Project />
+        </div>
     </main>
 </template>
 
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue';
+import LFSProject from '@/components/LFS-Project.vue';
 </script>
+
+<style scoped>
+.span {
+    color: #0ef;
+}
+
+.title-container {
+    position: fixed;
+    top: 40%;
+    left: 0%;
+    right: 50%;
+    display: flex;
+    justify-content: center;
+}
+
+.title {
+    max-width: 60%;
+    text-align: left;
+}
+
+.projects {
+    margin-left: 50%;
+    display: flex;
+    flex-direction: column;
+}
+</style>
