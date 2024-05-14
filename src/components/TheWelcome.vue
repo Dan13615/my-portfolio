a45bf8d0effb41156ab7806b83ad17a258072ba7
Dan13615 <daniil.stepanov@epitech.eu>
2024-05-14 17:27:50 +0200

    [~] Welcome view css -> tailwind

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -41,9 +41,9 @@ export default {
 
 <template>
     <WelcomeItem>
-        <div class="centered-container">
-            <div class="content-container">
-                <div class="text-container">
+        <div class="flex justify-center items-center px-40">
+            <div class="flex flex-row">
+                <div class="w-1/2">
                     <div>
                         <p class="text-xl md:text-xl lg:text-5xl">Hello I'm</p>
                         <p class="text-2xl md:text-4xl lg:text-7xl">{{ name }}</p>
@@ -52,17 +52,17 @@ export default {
                         </p>
                     </div>
 
-                    <div class="sections-container">
-                        <p class="title text-primary text-xl md:text-xl lg:text-5xl font-bold">About Me:</p>
+                    <div class="mt-20">
+                        <p class="mb-4 text-primary text-xl md:text-xl lg:text-5xl font-bold">About Me:</p>
                         <p class="text-xs md:text-sm lg:text-base">{{ aboutme }}</p>
                     </div>
 
-                    <div class="sections-container">
+                    <div class="mt-20">
                         <p class="title text-primary text-xl md:text-xl lg:text-5xl font-bold">Experience:</p>
                         <p class="text-xs md:text-sm lg:text-base">{{ exp }}</p>
                     </div>
 
-                    <div class="sections-container">
+                    <div class="mt-20">
                         <p class="title text-primary text-xl md:text-xl lg:text-5xl font-bold">Skills:</p>
                         <li class="text-xs md:text-sm lg:text-base">Proficient in C/C++ for low-level programming.</li>
                         <li class="text-xs md:text-sm lg:text-base">Extensive experience with embedded systems development.</li>
@@ -78,151 +78,27 @@ export default {
 
                 <div class="px-10" v-show="isNotSmallScreen"></div>
 
-                <div class="assets-container flex-1 text-center">
-                    <div class="social-media-links">
-                        <p class="text-xl md:text-xl lg:text-5xl">My social links :</p>
-                        <a class="social-media text-primary" href="https://www.facebook.com/daniil.stepanov.311">
+                <div class="fixed left-2/3 top-1/2 flex-1 text-center">
+                    <div class="social-media-links mb-10">
+                        <p class="text-xl md:text-xl lg:text-5xl mb-10">My social links :</p>
+                        <a class="inline-flex justify-center items-center w-16 h-16 bg-transparent border-primary border-solid border-4 rounded-full m-3 text-4xl text-primary shadow-custom2 transition duration-150 ease-out hover:ease-in hover:shadow-none" href="https://www.facebook.com/daniil.stepanov.311">
                             <font-awesome-icon :icon="['fab', 'facebook-f']" />
                         </a>
-                        <a class="social-media text-primary" href="https://www.instagram.com/dan13615_/">
+                        <a class="inline-flex justify-center items-center w-16 h-16 bg-transparent border-primary border-solid border-4 rounded-full m-3 text-4xl text-primary shadow-custom2 transition duration-150 ease-out hover:ease-in hover:shadow-none" href="https://www.instagram.com/dan13615_/">
                             <font-awesome-icon :icon="['fab', 'instagram']" />
                         </a>
-                        <a class="social-media text-primary" href="https://www.linkedin.com/in/daniil-stepanov-89a98715a">
+                        <a class="inline-flex justify-center items-center w-16 h-16 bg-transparent border-primary border-solid border-4 rounded-full m-3 text-4xl text-primary shadow-custom2 transition duration-150 ease-out hover:ease-in hover:shadow-none" href="https://www.linkedin.com/in/daniil-stepanov-89a98715a">
                             <font-awesome-icon :icon="['fab', 'linkedin']" />
                         </a>
-                        <a class="social-media text-primary" href="https://www.github.com/Dan13615/">
+                        <a class="inline-flex justify-center items-center w-16 h-16 bg-transparent border-primary border-solid border-4 rounded-full m-3 text-4xl text-primary shadow-custom2 transition duration-150 ease-out hover:ease-in hover:shadow-none" href="https://www.github.com/Dan13615/">
                             <font-awesome-icon :icon="['fab', 'github']" />
                         </a>
                     </div>
-                    <a href="cv-daniil.pdf" download="CV-Daniil-Stepanov" class="button">Download CV</a>
+                    <a href="cv-daniil.pdf" download="CV-Daniil-Stepanov" class="inline-block px-10 py-6 bg-primary rounded-full shadow-custom1 font-black text-3xl tracking-widest transition duration-150 ease-out hover:ease-in hover:shadow-none">Download CV</a>
                 </div>
             </div>
         </div>
     </WelcomeItem>
 </template>
 
-<style scoped>
-.centered-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-left: 10%;
-    padding-right: 10%;
-}
-
-.text-container {
-    width: 50%;
-}
-
-.assets-container {
-    position: fixed;
-    right: 0;
-    left: 50%;
-    top: 55%;
-    transform: translateY(-60%);
-}
-
-.sections-container {
-    margin-top: 5rem;
-}
-
-.title {
-    font-weight: 700;
-    margin-bottom: 1rem;
-}
-
-.social-media {
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    width: 4rem;
-    height: 4rem;
-    background: transparent;
-    border: 0.2rem solid #0ef;
-    border-radius: 50%;
-    font-size: 2rem;
-    margin: 3rem 1.5rem 3rem 0.2rem;
-    transition: 0.5s ease;
-}
-
-.social-media a:hover {
-    background: #0ef;
-    color: rgb(255, 255, 255);
-    box-shadow: 0 0 1rem #0ef;
-}
-
-.button {
-    display: inline-block;
-    padding: 1rem 2.8rem;
-    background: #0ef;
-    border-radius: 4rem;
-    box-shadow: 0 0 1rem #0ef;
-    font-size: 1.6rem;
-    color: rgb(255, 255, 255);
-    letter-spacing: 0.1rem;
-    font-weight: 1000;
-    transition: 0.5s ease;
-}
-
-.button:hover {
-    box-shadow: none;
-}
-
-.content-container {
-    display: flex;
-    flex-direction: row;
-}
-
-@media (max-width: 1024px) {
-    .content-container {
-        flex-direction: column;
-        text-align: center;
-    }
-
-    .button {
-        background: #0ef;
-        box-shadow: 0 0 0.7rem #0ef;
-        color: rgb(255, 255, 255);
-        display: inline-block;
-        padding: 0.3rem 1rem;
-        border-radius: 4rem;
-        font-size: 1rem;
-        letter-spacing: 0.1rem;
-        font-weight: 1000;
-        transition: 0.5s ease;
-    }
-
-    .social-media {
-        color: #0ef;
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        width: 2rem;
-        height: 2rem;
-        background: transparent;
-        border: 0.2rem solid #0ef;
-        border-radius: 50%;
-        font-size: 1rem;
-        margin: 2rem 1rem 2rem 1rem;
-        transition: 0.5s ease;
-    }
-
-    .social-media a:hover {
-        background: #0ef;
-        color: rgb(255, 255, 255);
-        box-shadow: 0 0 1rem #0ef;
-    }
-
-    .text-container {
-        width: 100%;
-    }
-
-    .assets-container {
-        position: relative;
-        right: 0;
-        left: 0;
-        top: 0;
-        transform: translateY(0);
-    }
-}
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                custom1: '0 0 1rem 0.5rem #0ef',
+                custom2: '0 0 0.7rem 0.1rem #0ef'
+            }
+        },
         colors: {
             primary: '#0ef'
         }
