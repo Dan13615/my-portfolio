029fc809671cd717db8b26e113e98fd185e6c96d
Dan13615 <daniil.stepanov@epitech.eu>
2024-06-02 08:41:27 +0200

    [+] responsiv contact page

--- src/components/TheContact.vue
+++ src/components/TheContact.vue
@@ -15,11 +15,11 @@ const linkedin = ref('https://www.linkedin.com/in/daniil-stepanov-89a98715a');
 
 <template>
     <ContactItem>
-        <div class="flex">
-            <div>
-                <p class="font-bold text-4xl md:text-6xl lg:text-8xl">Contact <span class="text-primary">Me !</span></p>
+        <div class="flex flex-col md:flex-row px-6">
+            <div class="text-center md:text-left md:pr-6">
+                <p class="font-bold text-5xl md:text-6xl lg:text-8xl">Contact <span class="text-primary">Me !</span></p>
             </div>
-            <div class="text-center w-3/5">
+            <div class="text-center pt-20 w-full md:w-3/5 md:pt-0">
                 <div class="social-media-links mb-10">
                     <a class="inline-flex justify-center items-center w-16 h-16 bg-transparent border-primary border-solid border-4 rounded-full m-3 text-4xl text-primary shadow-custom2 transition duration-150 ease-out hover:ease-in hover:shadow-none" :href="facebook">
                         <font-awesome-icon :icon="['fab', 'facebook-f']" />
@@ -36,7 +36,7 @@ const linkedin = ref('https://www.linkedin.com/in/daniil-stepanov-89a98715a');
                 </div>
                 <a href="/cv-daniil.pdf" download="CV-Daniil-Stepanov" class="inline-block px-10 py-6 bg-primary rounded-full shadow-custom2 font-black text-3xl tracking-widest transition duration-150 ease-out hover:ease-in hover:shadow-none">Download CV</a>
                 <p class="py-10 text-lg md:text-xl lg:text-2xl">
-                    <a class="text-primary" :href="`mailto:${my_email}`"> {{ email }} </a>
+                    <a class="text-primary text-2xl md:text-3xl" :href="`mailto:${my_email}`"> {{ email }} </a>
                 </p>
             </div>
         </div>
