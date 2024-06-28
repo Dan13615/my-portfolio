f2eea9141d0db2ff649138d7830ceaaa3d089e01
Dan13615 <daniil.stepanov@epitech.eu>
2024-06-28 11:18:37 +0200

    [+] working on new UI

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import TheHeader from './components/TheHeader.vue';
 </script>
 
 <template>
-    <div class="text-sm lg:text-xl">
-        <header class="p-2 w-full flex flex-row justify-between fixed">
-            <div>
-                <RouterLink to="/" class="font-bold">HOME</RouterLink>
-            </div>
-            <div>
-                <nav class="font-bold w-full flex gap-5">
-                    <RouterLink to="/projects">PROJECTS</RouterLink>
-                    <RouterLink to="/contact">CONTACT</RouterLink>
-                </nav>
-            </div>
-        </header>
-    </div>
-    <div class="min-h-screen flex justify-center transition-all padd">
+    <TheHeader />
+    <div class="min-h-screen flex justify-center transition-all">
         <RouterView />
     </div>
 </template>
 
-<style scoped>
-header {
-    background-color: rgba(57, 62, 70, 0.8);
-}
-
-nav {
-    color: #eeeeee;
-}
-
-.padd {
-    padding-top: 8rem;
-}
-
-@media (min-width: 1024px) {
-    .padd {
-        padding-top: 16rem;
-    }
-}
-</style>
+<style></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <HeaderItem>
+        <div class="text-sm lg:text-xl">
+            <div class="p-2 w-full flex flex-row justify-between fixed">
+                <div>
+                    <RouterLink to="/" class="font-bold">HOME</RouterLink>
+                </div>
+                <div>
+                    <nav class="font-bold w-full flex gap-5">
+                        <RouterLink to="/projects">PROJECTS</RouterLink>
+                        <RouterLink to="/contact">CONTACT</RouterLink>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </HeaderItem>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
