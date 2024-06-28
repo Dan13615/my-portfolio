ee24f77b9413de3239f87036a29a01481b98348e
Dan13615 <daniil.stepanov@epitech.eu>
2024-06-28 11:02:22 +0200

    [-] updated

--- src/router/index.js
+++ src/router/index.js
@@ -18,11 +18,6 @@ const router = createRouter({
             path: '/projects',
             name: 'projects',
             component: () => import('../views/ProjectsView.vue')
-        },
-        {
-            path: '/maman',
-            name: 'maman',
-            component: () => import('../views/MamanView.vue')
         }
     ]
 });
